e311a341c335f4354811dbb302e0bb93ae13b417
e.saci <saciedip@gmail.com>
2023-10-16 13:53:04 +0300

    chromedriver.exe added and Driver.java updated

--- src/test/java/com/automationtesting/utilities/Driver.java
+++ src/test/java/com/automationtesting/utilities/Driver.java
@@ -3,77 +3,56 @@ package com.automationtesting.utilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
     /*
-    Creating a private constructor, so we are closing
-    access to the object of this class from outside the class
-     */
-    private Driver(){}
-
-    /*
-    We make WebDriver private, because we want to close access from outside the class.
-    We make it static because we will use it in a static method.
-     */
-    private static WebDriver driver; // value is null by default
+    We make WebDriver private, because we want to  close
+    access from outside of this class
+    We make it static because we will use it in a static method
+    */
+    //private static WebDriver driver;
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
     /*
     Create a re-usable utility method which will return same driver instance when we call it
      */
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if (driver == null){
+        if (driverPool.get() == null) {
             /*
-            We read our browserType from configuration.properties.
-            This way, we can control which browser is opened from outside our code, from configuration.properties.
+            We read our browserType from configuration.properties
+            This way, we can control which browser is opened from our code,
+            from configuration.properties
              */
+
             String browserType = ConfigurationReader.getProperty("browser");
 
             /*
                 Depending on the browserType that will be return from configuration.properties file
-                switch statement will determine the case, and open the matching browser
-            */
-            switch (browserType){
-
+                switch statement will determine the case and open the matching browser
+             */
+            switch (browserType) {
                 case "chrome":
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+                    driverPool.set(new ChromeDriver());
                     break;
-
-                case "safari" :
-                    driver=new SafariDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-                    break;
-
                 case "firefox":
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                    driverPool.set(new FirefoxDriver());
                     break;
-
-                default:
-                    System.out.println("Unknown browser type: "+browserType);
-                    driver=null;
-
             }
-        }
 
-        return driver;
+        }
+        return driverPool.get();
     }
 
-    /*
-    This method will make sure our driver value is always null after using quit() method
-     */
-    public static void closeDriver(){
-        if (driver != null){
-            driver.quit(); // this line will terminate the existing session. Value will not even be null
-            driver = null;
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
+            driverPool.get().quit();//this line terminate the existing session.
+            // But Value will not even be null
+            driverPool.remove();
         }
+
     }
+
 }
